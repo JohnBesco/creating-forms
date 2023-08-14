e9b704e968345a4150eade244e3fd455943d251c
SirTokenSol <johnbesco.dev@gmail.com>
2023-08-14 22:33:30 +0100

    Create const for checkPasswords func

---
+++ app.js
@@ -0,0 +1,7 @@
+
+
+function checkPasswords() {
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirm-password").value;
+    const error = document.getElementById("password-error");
+}
