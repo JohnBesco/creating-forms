7e2fcc4ffc1d780f9536d55c96cac02783ba9c8b
SirTokenSol <johnbesco.dev@gmail.com>
2023-08-14 22:39:11 +0100

    Add check password logic

--- app.js
+++ app.js
@@ -4,4 +4,17 @@ function checkPasswords() {
     const password = document.getElementById("password").value;
     const confirmPassword = document.getElementById("confirm-password").value;
     const error = document.getElementById("password-error");
+
+    if (password !== confirmPassword) {
+        error.textContent = "Passwords do not match";
+        error.style.color = "#e74c3c";
+        confirmPassword.classList.add("error");
+        return false;
+      } else {
+        error.textContent = "";
+        error.style.color = "";
+        confirmPassword.classList.remove("error");
+        return true;
+    }
 }
+  
